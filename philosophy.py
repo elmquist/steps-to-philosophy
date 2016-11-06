adccb82521fc8538dcdf7dc42a3e6c8448a74e36
Ryan Elmquist <ryan.elmquist@gmail.com>
2016-11-06 11:48:18 -0800

    fixing main loop to give better output

--- philosophy.py
+++ philosophy.py
@@ -6,16 +6,17 @@ import wiki
 def FindPage(start, end='Philosophy'):
   current = start
   path = []
-  while current.lower() != end.lower():
+  while True:
+    print('-> %s' % current)
     path.append(current)
+    if current.lower() == end.lower():
+      break
     if path.count(current) > 1:
       print('found a loop!')
       print(path)
       break
-    print('WIKI PAGE %s' % current)
     current = wiki.ProcessTitle(current)
 
 
 if __name__ == '__main__':
-  print(FindPage(sys.argv[1]))
-
+  FindPage(sys.argv[1])
